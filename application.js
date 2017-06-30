a65ec9e346ddda3d0a092c3f12eddb2d97f72473
Hitomi Sugiyama <hitomisugiyama@allkaitori-no-MacBook-Pro.local>
2017-06-30 18:40:07 +0900

    Initial commit

---
+++ application.js
@@ -0,0 +1,45 @@
+window.addEventListener('load', () => {
+  let nihon = document.getElementById('nihon');
+    console.log("");
+  document.getElementById('kita').addEventListener('click', (e) => {
+    nihon.src = "images/kita1.png";
+    e.preventDefault();
+  }); 
+    
+  let nihon = document.getElementById('nihon');
+    console.log("");
+  document.getElementById('kantou').addEventListener('click', (e) => {
+    nihon.src = "images/kantou1.png";
+    e.preventDefault();
+  }); 
+    
+  let nihon = document.getElementById('nihon');
+    console.log("");
+  document.getElementById('tyubu').addEventListener('click', (e) => {
+    nihon.src = "images/tyubu1.png";
+    e.preventDefault();
+  }); 
+  
+  let nihon = document.getElementById('nihon');
+    console.log("");
+  document.getElementById('kinki').addEventListener('click', (e) => {
+    nihon.src = "images/kinki1.png";
+    e.preventDefault();
+  }); 
+  
+  let nihon = document.getElementById('nihon');
+    console.log("");
+  document.getElementById('setonai').addEventListener('click', (e) => {
+    nihon.src = "images/setonai1.png";
+    e.preventDefault();
+  }); 
+  
+  let nihon = document.getElementById('nihon');
+    console.log("");
+  document.getElementById('kyusyu').addEventListener('click', (e) => {
+    nihon.src = "images/kyusyu1.png";
+    e.preventDefault();
+  }); 
+  
+
+});
